aa8f1d40d47768053236bc2f79e0344fa04a227d
thrombe <yash.n.thombare620@gmail.com>
2021-04-28 01:48:32 +0530

    changed stuff to float where int isnt required

---
+++ go.mod
@@ -0,0 +1,3 @@
+module termiRen
+
+go 1.16
--- projection.go
+++ projection.go
@@ -7,8 +7,8 @@ import (
 func main() {
     board := genB()
     point(0, 0, board)
-    x, y := rotateP(10, 10, 0, 0, 3.1 )
-    point(round(x), round(y), board)
+    x, y := rotateP(10, 10, 0, 0, 0.0)
+    point(x, y, board)
     // point(-10, -10, board )
     vector(1, 2, 30, 11, board)
     printB(board)
@@ -41,14 +41,14 @@ func printB(board []int) {
     fmt.Printf(scr)
 }
 
-func line(x1 int, y1 int, x2 int, y2 int, board []int) {
-    length := math.Sqrt(float64((x2-x1)*(x2-x1) + (y2-y1)*(y2-y1)))
+func line(x1, y1, x2, y2 float64, board []int) {
+    length := math.Sqrt((x2-x1)*(x2-x1) + (y2-y1)*(y2-y1))
     vx, vy := float64(x2-x1), float64(y2-y1)
     ux, uy := vx/length,  vy/length
     x, y, x3 := float64(x1), float64(y1), float64(x2)
     for x <= x3 {
         //x, y = math.Round(x), math.Round(y)
-        point(round(x), round(y), board)
+        point(x, y, board)
         x, y = x+ux, y+uy
     }
 }
@@ -57,12 +57,12 @@ func matMul() {
     fmt.Printf("matMul not implemented")
 }
 
-func vector(x1 int, y1 int, x2 int, y2 int, board []int) {
+func vector(x1, y1, x2, y2 float64, board []int) {
     line(x1, y1, x1+x2, y1+y2, board)
 }
 
-func point(x int, y int, board []int) {
-    x, y = giveInd(x, y)
+func point(h, k float64, board []int) {
+    x, y := giveInd(h, k)
     board[x+y*xlim] = 1
 }
 
@@ -77,12 +77,12 @@ func genB() []int {
     // }
 }
 
-func giveCoords(x int, y int) (int, int) {
-    return x-int(xlim/2), -y+int(ylim/2)
+func giveCoords(x, y int) (float64, float64) {
+    return float64(x-int(xlim/2)), float64(-y+int(ylim/2)) 
 }
 
-func giveInd(x int, y int) (int, int) {
-    return x+int(xlim/2), -y+int(ylim/2)
+func giveInd(x, y float64) (int, int) {
+    return round(x)+int(xlim/2), -round(y)+int(ylim/2)
 }
 
 func round(i float64) int {
