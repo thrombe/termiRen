f96ad96bd24cd0d14a6dd5f018f85f0cafec739b
thrombe <yash.n.thombare620@gmail.com>
2021-04-28 18:28:43 +0530

    added a bunch of matrix/vector manipulation functions

---
+++ mathStuff.go
@@ -0,0 +1,127 @@
+package main
+import (
+    // "fmt"
+    "math"
+)
+
+/*multiply matrices uf arbitary sizes(legal only ofc)*/
+func matMul(mat1, mat2 [][]float64) [][]float64 {
+    m1rows, m1cols, m2rows, m2cols := len(mat1), len(mat1[0]), len(mat2), len(mat2[0])
+    if m1rows != m2cols {panic("matMul shape error")}
+    result := make([][]float64, m1rows)
+    for r := 0; r < m1rows; r++ {
+        row := make([]float64, m2rows)
+        for c := 0; c < m2cols; c++ {
+            for item := 0; item < m1cols; item++ {
+                row[item] += mat1[r][item]*mat2[item][c]
+            }
+        }
+        result[r] = row
+    }
+    return result
+}
+
+func matAdd(mat1, mat2 [][]float64) [][]float64 {
+    m1rows, m1cols, m2rows, m2cols := len(mat1), len(mat1[0]), len(mat2), len(mat2[0])
+    if !(m1rows == m2rows && m1cols == m2cols) {panic("matAdd shape error")}
+    result := make([][]float64, m1rows)
+    for r := 0; r < m1rows; r++ {
+        row := make([]float64, m1cols)
+        for c := 0; c < m1cols; c++ {
+            row[c] = mat1[r][c] + mat2[r][c]
+        }
+        result[r] = row
+    }
+    return result
+}
+
+func matSub(mat1, mat2 [][]float64) [][]float64 {
+    m1rows, m1cols, m2rows, m2cols := len(mat1), len(mat1[0]), len(mat2), len(mat2[0])
+    if !(m1rows == m2rows && m1cols == m2cols) {panic("matSub shape error")}
+    result := make([][]float64, m1rows)
+    for r := 0; r < m1rows; r++ {
+        row := make([]float64, m1cols)
+        for c := 0; c < m1cols; c++ {
+            row[c] = mat1[r][c] - mat2[r][c]
+        }
+        result[r] = row
+    }
+    return result
+}
+
+func matScale(mat [][]float64, scale float64) [][]float64 {
+    mrows, mcols := len(mat), len(mat[0])
+    for r := 0; r < mrows; r++ {
+        for c := 0; c < mcols; c++ {
+            mat[r][c] = mat[r][c]*scale
+        }
+    }
+    return mat
+}
+
+/*remember to input y and x index resp. *///returns -1^(x+y)*submat */
+func subMat(mat [][]float64, y, x int) [][]float64 {
+    
+    mrows := len(mat)
+    var submat [][]float64
+    for r := 0; r < mrows; r++ {
+        if r == y {continue}
+        var row []float64
+        for c := 0; c < mrows; c++ {
+
+            if c == x {continue}
+            row = append(row, mat[r][c])
+        }
+        
+        submat = append(submat, row)
+    }
+    return submat
+    //return matScale(submat, math.Pow(-1, float64(x+y)))
+}
+
+/*returns determinant of a square matrix*/
+func matDet(mat [][]float64) float64 {
+    mrows, mcols := len(mat), len(mat[0])
+    if mrows != mcols {panic("matDet non square matrix")}
+    if mrows == 2 {return mat[0][0]*mat[1][1] - mat[0][1]*mat[1][0]}
+    submat := make([][]float64, mrows-1)
+    var result float64
+    for r := 0; r < mrows; r++ {
+        for c := 0; c < mrows; c++ {
+            result += mat[r][c] * math.Pow(-1, float64(c)) * matDet(submat)
+        }
+    }
+
+    panic("matDet not imllemented")
+    return 0
+}
+
+/*returns the size of a n-dimentional vector*/
+func vecSize(vec [][]float64) float64 {
+    if len(vec[0]) > 1 {panic("vecSize not a vector")}
+    vecDimensions := len(vec)
+    var result float64
+    for r := 0; r < vecDimensions; r++ {
+        result += vec[r][0] * vec[r][0]
+    }
+    return math.Sqrt(result)
+}
+
+/*returns dot of 2 vectors*/
+func vecDot(vec1, vec2 [][]float64) float64 {
+    if len(vec1[0]) > 1 || len(vec2) > 1 {panic("vecDot not a vector")}
+    if len(vec1) != len(vec2) {panic("vecDot vectors of different dimentions")}
+    vecDimensions := len(vec1)
+    var result float64
+    for r := 0; r < vecDimensions; r++ {
+        result += vec1[r][0] * vec2[r][0]
+    }
+    return result
+}
+
+/*rounds to nearest int and retrun int*/
+func round(i float64) int {
+    return int(math.Round(i))
+    //llim := int(i)
+    //if i-float64(llim) >= 0.5 {return llim+1} else {return llim}
+}
--- projection.go
+++ projection.go
@@ -5,18 +5,27 @@ import (
     )
 
 func main() {
-    board := genB()
-    point(0, 0, board)
-    x, y := rotateP(10, 10, 0, 0, 0.0)
-    point(x, y, board)
+    // board := genB()
+    // point(0, 0, board)
+    // x, y := rotateP(10, 10, 0, 0, 0.0)
+    // point(x, y, board)
     // point(-10, -10, board )
-    vector(1, 2, 30, 11, board)
-    printB(board)
+    // vector(1, 2, 30, 11, board)
+    // printB(board)
+    mat1 := [][]float64 {
+        {3, 1, 4, 6}, 
+        {1, 4, 6, 3}, 
+        {6, 3, 1, 9},
+        {2, 8, 5, 8} }
+    // fmt.Println(mat1[0][0])
+    new := subMat(mat1, 2, 3)
+    fmt.Println(new)
 }
 
 const xlim = 151
 const ylim = 160
 
+/*rotate x, y about ox, oy by r radians*/
 func rotateP(x, y, ox, oy, r float64) (float64, float64) {
     x, y = x-ox, y-oy
     x, y =  x*math.Cos(r)-y*math.Sin(r), x*math.Sin(r)+y*math.Cos(r)
@@ -24,12 +33,7 @@ func rotateP(x, y, ox, oy, r float64) (float64, float64) {
 }
 
 func printB(board []int) {
-    //xlim, ylim := 150, 160
-    // nxlim, nylim := -int(xlim/2), -int(ylim/2)
-    // pxlim, pylim := xlim+nxlim, ylim+nylim
     var scr string
-    // for y := nylim; y <= pylim; y++ {
-        // for x := nxlim; x <= pxlim; x++ {
     for y := 0; y < ylim; y++ {
         for x := 0; x < xlim; x++ {
             if board[x+y*xlim] == 1 {
@@ -38,34 +42,33 @@ func printB(board []int) {
         }
         scr += "\n"
     }
-    fmt.Printf(scr)
+    fmt.Println(scr)
 }
 
+/*draw line on canvas*/
 func line(x1, y1, x2, y2 float64, board []int) {
     length := math.Sqrt((x2-x1)*(x2-x1) + (y2-y1)*(y2-y1))
     vx, vy := float64(x2-x1), float64(y2-y1)
     ux, uy := vx/length,  vy/length
     x, y, x3 := float64(x1), float64(y1), float64(x2)
     for x <= x3 {
-        //x, y = math.Round(x), math.Round(y)
         point(x, y, board)
         x, y = x+ux, y+uy
     }
 }
 
-func matMul() {
-    fmt.Printf("matMul not implemented")
-}
-
+/*draw vector with x1, y1 as offset and x2, y2 as direction*/
 func vector(x1, y1, x2, y2 float64, board []int) {
     line(x1, y1, x1+x2, y1+y2, board)
 }
 
+/*draw point on canvas*/
 func point(h, k float64, board []int) {
     x, y := giveInd(h, k)
     board[x+y*xlim] = 1
 }
 
+/*make a canvas. might be useful to use float to be able to have brightness and stuff*/
 func genB() []int {
     return make([]int, xlim*ylim)
     // var board [][]int
@@ -77,16 +80,13 @@ func genB() []int {
     // }
 }
 
+/*enter index of pixel in canvas and return coors in space*/
 func giveCoords(x, y int) (float64, float64) {
     return float64(x-int(xlim/2)), float64(-y+int(ylim/2)) 
 }
 
+/*enter coords of points and return index in canvas*/
 func giveInd(x, y float64) (int, int) {
     return round(x)+int(xlim/2), -round(y)+int(ylim/2)
 }
 
-func round(i float64) int {
-    return int(math.Round(i))
-    //llim := int(i)
-    //if i-float64(llim) >= 0.5 {return llim+1} else {return llim}
-}
